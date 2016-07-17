fe1a7778dc1b2c6db634ca13a1b243e4158c8d61
sokratis12GR <sokratis12GR@users.noreply.github.com>
2016-07-17 13:12:54 +0300

    Update GlobalChat.java

--- GlobalChat/src/main/java/net/thedragonteam/globalchat/GlobalChat.java
+++ GlobalChat/src/main/java/net/thedragonteam/globalchat/GlobalChat.java
@@ -12,11 +12,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = GlobalChat.MODID, version = GlobalChat.VERSION)
+@Mod(modid = GlobalChat.MODID, version = GlobalChat.VERSION, name = GlobalChat.NAME)
 public class GlobalChat
 {
     public static final String MODID = "globalchat";
     public static final String VERSION = "1.0";
+    public static final String NAME = "GlobalChat";
     
     @EventHandler
 	public void preInit(FMLPreInitializationEvent event)  {
